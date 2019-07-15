d840171a1f7b276c32d7baf17b959c13f4bbac24
Thomas Viles <thomas.c.viles.jr@gmail.com>
2019-07-15 12:54:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,16 @@
-class Student 
+require "./user.rb"
+class Student < User
+  attr_accessor :knowledge
 
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(info)
+    @knowledge<<info
+  end
+
+  def knowledge
+    @knowledge
+  end
 end
